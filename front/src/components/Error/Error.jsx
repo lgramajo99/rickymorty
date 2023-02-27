5fd52e4e13b64eee5b4f6b3517d0ab19d45c512a
lgramajo99 <lucianongramajo@gmail.com>
2023-02-27 16:59:11 -0300

    Listo

--- front/src/components/Error/Error.jsx
+++ front/src/components/Error/Error.jsx
@@ -1,6 +1,9 @@
 function Error() {
-    return (<div>
-        <h1>Error: esta pagina no existe</h1>
+    const _error = 'https://i.imgur.com/ZIQQXfV.png'
+
+    return (<div >
+        {/* <h1>Error: esta pagina no existe</h1> */}
+        <img src={_error} alt="Error 404" />
     </div>)
 }
 
