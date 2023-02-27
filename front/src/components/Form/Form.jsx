16778b093c466a7a17e1ed3bd91b0f4ed324a841
lgramajo99 <lucianongramajo@gmail.com>
2023-02-27 16:45:09 -0300

    form listo

--- front/src/components/Form/Form.jsx
+++ front/src/components/Form/Form.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import validation from "./validation";
+import style from './Form.module.css';
+
 
 const Form = ({ login }) => {
 
@@ -30,18 +32,18 @@ const Form = ({ login }) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="username" >Usuario: </label>
-            <input type="text" name="username" value={userData.username} onChange={handleInputChange} />
-            {errors.username && <p>{errors.username}</p>}
+        <form className={style.formContenedor} onSubmit={handleSubmit}>
+            <label className={style.formLabel} htmlFor="username" >Usuario</label>
+            <input className={style.formInput} type="text" name="username" value={userData.username} onChange={handleInputChange} />
+            {errors.username && <p className={style.formError}>{errors.username}</p>}
 
             <br />
-            <label htmlFor="password" >Contraseña: </label>
-            <input type="password" name="password" value={userData.password} onChange={handleInputChange} />
-            {errors.password && <p>{errors.password}</p>}
+            <label className={style.formLabel} htmlFor="password" >Contraseña</label>
+            <input className={style.formInput} type="password" name="password" value={userData.password} onChange={handleInputChange} />
+            {errors.password && <p className={style.formError}>{errors.password}</p>}
 
             <br />
-            <button type="submit">Entrar</button>
+            <button className={style.formBtn} type="submit">Entrar</button>
         </form>
     )
 }
